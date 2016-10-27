151f47058f205f4ccac44a781f833408d669e874
Yusu Pan <xxdsox@gmail.com>
2016-10-28 00:06:58 +0800

    Try Deeper LSTM.
    Still bad result.
    Maybe I should use a more detailed dataset.

--- main.py
+++ main.py
@@ -2,17 +2,15 @@ from __future__ import print_function
 import numpy as np
 import matplotlib.pyplot as plt
 from keras.models import Sequential
-from keras.layers import Dense, LSTM, Activation
+from keras.layers import Dense, LSTM, Activation, Dropout
+from random import uniform
 from utils import data_loader, train_test_split
+# Fix AttributeError: 'module' object has no attribute 'control_flow_ops'
+import tensorflow
+from tensorflow.python.ops import control_flow_ops
+tensorflow.python.control_flow_ops = control_flow_ops
 
 if __name__ == '__main__':
-    # since we are using stateful rnn tsteps can be set to 1
-    tsteps = 1
-    batch_size = 24
-    epochs = 50
-    # number of elements ahead that are used to make the prediction
-    lahead = 1
-
     print('-- Loading Data --')
     X, y = data_loader('data/data302.csv')
     X_train, y_train, X_test, y_test = train_test_split(X, y)
@@ -20,20 +18,41 @@ if __name__ == '__main__':
     print('Output shape:', y.shape)
 
     print('--Creating Model--')
-    in_out_neurons = 1
+    batch_size = 24
+    epochs = 100
+    out_neurons = 1
     hidden_neurons = 500
+    dropout = uniform(0, 0.5)
 
     model = Sequential()
-    model.add(LSTM(hidden_neurons, input_shape=(5, 1)))
-    model.add(Dense(in_out_neurons))
-    model.add(Activation("linear"))
-
-    model.compile(loss="mean_squared_error",
-        optimizer="adam",
-        metrics=['accuracy'])
+    model.add(LSTM(output_dim=hidden_neurons,
+                   input_dim=X_train.shape[2],
+                   init='uniform',
+                   return_sequences=True
+                   ))
+    model.add(Dropout(dropout))
+    model.add(LSTM(output_dim=hidden_neurons,
+                   input_dim=hidden_neurons,
+                   return_sequences=False
+                   ))
+    model.add(Dropout(dropout))
+    model.add(Activation('relu'))
+    model.add(Dense(output_dim=out_neurons, input_dim=hidden_neurons))
+    model.add(Activation('relu'))
+    model.compile(loss="mse",
+                  optimizer="adam",
+                  metrics=['accuracy'])
 
     print('-- Training --')
-    model.fit(X_train, y_train, batch_size=24, nb_epoch=50, validation_data=(X_test, y_test), verbose=1)
+    history = model.fit(X_train, y_train,
+                        verbose=1,
+                        batch_size=batch_size,
+                        nb_epoch=epochs,
+                        validation_split=0.3,
+                        shuffle=False
+                        )
+
+    print('--Testing--')
     score = model.evaluate(X_test, y_test, verbose=0)
     print('Test score:', score[0])
     print('Test accuracy:', score[1])
--- utils.py
+++ utils.py
@@ -9,14 +9,14 @@ def data_loader(filename):
 		reader = csv.DictReader(csvfile)
 		for row in reader:
 			date = datetime.strptime(row['Time'], '%Y-%m-%d  %H:%M:%S')
-			X.append([date.hour, row['Route'], row['Weather'], row['Roadtype'], row['Holiday']])
+			X.append([date.hour, row['Weather'], row['Holiday']])
 			y.append([row['Velocity']])
 	X = np.array(X)
-	X = X.reshape(X.shape[0], X.shape[1], 1)
+	X = X.reshape(X.shape[0], 1, X.shape[1])
 	y = np.array(y)
 	return X, y
 
-def train_test_split(X, y, test_size=24):
+def train_test_split(X, y, test_size=96):
     if X.shape[0] != y.shape[0]:
         raise ValueError("X and y must have the same number of rows")
     split_idx = X.shape[0] - test_size
