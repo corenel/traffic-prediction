058b681c5e0077669b3f8584efd1eaefa6859ea5
Yusu Pan <xxdsox@gmail.com>
2016-10-27 15:20:43 +0800

    Bad Try. Almost zero accuracy.

--- main.py
+++ main.py
@@ -1,20 +1,51 @@
+from __future__ import print_function
+import numpy as np
+import matplotlib.pyplot as plt
 from keras.models import Sequential
-from keras.layers.core import Dense, Activation
-from keras.layers.recurrent import LSTM
-from utils import data_loader
+from keras.layers import Dense, LSTM, Activation
+from utils import data_loader, train_test_split
 
+if __name__ == '__main__':
+    # since we are using stateful rnn tsteps can be set to 1
+    tsteps = 1
+    batch_size = 24
+    epochs = 50
+    # number of elements ahead that are used to make the prediction
+    lahead = 1
 
-X_train, y_train = data_loader('data/data302.csv')
-print X_train.shape, y_train.shape
+    print('-- Loading Data --')
+    X, y = data_loader('data/data302.csv')
+    X_train, y_train, X_test, y_test = train_test_split(X, y)
+    print('Input shape:', X.shape)
+    print('Output shape:', y.shape)
 
-in_out_neurons = 1
-hidden_neurons = 100
-model = Sequential()
-# n_prev = 100, 2 values per x axis
-model.add(LSTM(hidden_neurons, input_shape=(2952, 5)))
-model.add(Dense(in_out_neurons))
-model.add(Activation("linear"))
-model.compile(loss="mean_squared_error",
-    optimizer="rmsprop",
-    metrics=['accuracy'])
-model.fit(X_train, y_train, batch_size=72, nb_epoch=50, validation_split=0.1, verbose=1)
+    print('--Creating Model--')
+    in_out_neurons = 1
+    hidden_neurons = 500
+
+    model = Sequential()
+    model.add(LSTM(hidden_neurons, input_shape=(5, 1)))
+    model.add(Dense(in_out_neurons))
+    model.add(Activation("linear"))
+
+    model.compile(loss="mean_squared_error",
+        optimizer="adam",
+        metrics=['accuracy'])
+
+    print('-- Training --')
+    model.fit(X_train, y_train, batch_size=24, nb_epoch=50, validation_data=(X_test, y_test), verbose=1)
+    score = model.evaluate(X_test, y_test, verbose=0)
+    print('Test score:', score[0])
+    print('Test accuracy:', score[1])
+
+    # print('-- Predicting --')
+    # y_pred = model.predict(X_test, batch_size=batch_size)
+    #
+    # print('-- Plotting Results --')
+    # plt.subplot(2, 1, 1)
+    # plt.plot(y_test)
+    # plt.title('Expected')
+    # plt.subplot(2, 1, 2)
+    # plt.plot(y_pred)
+    # plt.title('Predicted')
+    # plt.show()
--- utils.py
+++ utils.py
@@ -12,7 +12,12 @@ def data_loader(filename):
 			X.append([date.hour, row['Route'], row['Weather'], row['Roadtype'], row['Holiday']])
 			y.append([row['Velocity']])
 	X = np.array(X)
+	X = X.reshape(X.shape[0], X.shape[1], 1)
 	y = np.array(y)
 	return X, y
 
-# def split_train_test_data(X, Y, num_train):
+def train_test_split(X, y, test_size=24):
+    if X.shape[0] != y.shape[0]:
+        raise ValueError("X and y must have the same number of rows")
+    split_idx = X.shape[0] - test_size
+    return X[:split_idx], y[:split_idx], X[split_idx:], y[split_idx:]
